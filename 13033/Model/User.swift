167fd3833a843d56e0fe19bb222322724663d6ec
Nick Athanasiou <athn.nick@gmail.com>
2020-04-14 19:58:30 +0300

    remove core data changed project target iOS to 10

---
+++ 13033/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  13033
+//
+//  Created by Nikolaos Athanasiou on 14/4/20.
+//  Copyright © 2020 Nikolaos Athanasiou. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var fullName: String
+    var address: String
+}
